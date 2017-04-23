bed687d4df1edb11f6f0dbe00bdfc7a268d321c8
_gt3 <gt3.someday@outlook.com>
2017-04-23 15:32:16 -0500

    new Container class closes #5

---
+++ src/container.js
@@ -0,0 +1,50 @@
+import { shieldProps, isClickValid } from './utils'
+import warning from 'warning'
+
+export class BrowserContainer {
+  static run(router) {
+    return new BrowserContainer(router).run()
+  }
+  constructor(router) {
+    this.history = new History()
+    this.router = router
+  }
+  run() {
+    let {history, router} = this
+    if (!this.handle) this.handle = history.listen(router.listen)
+  }
+  stop() {
+    if(this.handle) this.handle()
+  }
+  push(loc) {
+    warning(!!this.router.match(loc).spec, 'No paths defined for this location: %s', loc)
+    this.history.push(loc)
+  }
+  replace(loc) {
+    warning(!!this.router.match(loc).spec, 'No paths defined for this location: %s', loc)
+    this.history.replace(loc)
+  }
+}
+
+export const UltraLink = p => {
+  let props = shieldProps(p, 'createElement', 'container')
+  let { href, createElement, container } = props
+  props.onClick = createListener(container.push.bind(container, href))
+  return createElement('a', props)
+}
+UltraLink.defaultProps = {
+  style: {
+    cursor: 'pointer',
+    touchAction: 'manipulation',
+    msTouchAction: 'manipulation'
+  }
+}
+
+export function createListener(action) {
+  return function clickHandler(e) {
+    if (isClickValid) {
+      e.preventDefault()
+      action()
+    }
+  }
+}
