c768a01f59b39649443b320a221eeea4b325cb2b
Ankit Patel <gt3.someday@outlook.com>
2017-05-22 20:25:06 -0500

    encodePath generates warnings for qs, fragment, only applies encoding on path #32

--- src/utils-path.js
+++ src/utils-path.js
@@ -1,5 +1,5 @@
 import { pipe } from './utils'
-import warning from './warning'
+import warning from 'warning'
 
 function stripPrefix(prefix, path) {
   return prefix ? path.replace(prefix, '') : path
@@ -19,20 +19,40 @@ function normalizePath(prefix) {
 
 export { removeTrailingSlash, normalizePath }
 
+function verifyFragmentEncoding(f) {
+  return !f || verifyEncoding(f)
+}
+
 function extractFragment(path) {
   let [pathwof, ...f] = path.split(/\/#/)
-  warning(f.length <= 1, 'Ambiguous path detected. Could not parse fragment: %s => %s', path, f)
+  warning(f.length <= 1, 'Ambiguous path. Could not parse fragment: %s', f)
+  warning(verifyFragmentEncoding(f[0]), 'Incorrect encoding. Use encodeURI on fragment: %s', f[0])
   return [pathwof, f[0]]
+}
+
+function verifyQSEncoding(qs) {
+  return !qs || verifyEncoding(qs.replace(/=|&/g,''))
 }
 
 function extractQS(path) {
   let [pathwoqs, ...qs] = path.split(/\/\?/)
-  warning(qs.length <= 1, 'Ambiguous path detected. Could not parse query string: %s => %s', path, qs)
+  warning(qs.length <= 1, 'Ambiguous path. Could not parse query string: %s', qs)
+  warning(verifyQSEncoding(qs[0]), 'Incorrect encoding. Use encodeURIComponent on query string: %s', qs[0])
   return [pathwoqs, qs[0]]
 }
 
+function makePath(path, qs, f) {
+  let res = path
+  if(qs) res = res.concat('/?', qs)
+  if(f) res = res.concat('/#', f)
+  return res
+}
+
 function encodePath(path) {
-  return encodeURI(path).replace(/%5B/g, '[').replace(/%5D/g, ']')
+  let [pathwof, f] = extractFragment(path)
+  let [pathwoqs, qs] = extractQS(pathwof)
+  let encoded = encodeURI(pathwoqs).replace(/%5B/g, '[').replace(/%5D/g, ']')
+  return makePath(encoded, qs, f)
 }
 
 function decodePath(path) {
