93ca993efd772d3aabcd943526093070ef0583d1
_gt3 <gt3.someday@outlook.com>
2017-06-03 23:59:02 -0500

    parseHref test full url, encode

--- test/utils-path.test.js
+++ test/utils-path.test.js
@@ -2,6 +2,11 @@ import assert from 'assert'
 import { eq, neq, oeq, oneq, mock } from './helpers'
 import * as u from '../src/utils-path'
 
+let verifyMatch = (xhref, xpath, xqs, xhash) => actual => {
+  let { href, path, qs = '', hash = '' } = actual
+  return oeq([href, path, qs, hash], [xhref, xpath, xqs, xhash])
+}
+
 describe('path utils', function() {
   it('normalizeHref', function() {
     let norm = u.normalizeHref
@@ -67,17 +72,24 @@ describe('path utils', function() {
   })
   it('parseHref', function() {
     let parse = u.parseHref
-    let test = (xhref, xpath, xqs, xhash) => actual => {
-      let { href, path, qs = '', hash = '' } = actual
-      return oeq([href, path, qs, hash], [xhref, xpath, xqs, xhash])
-    }
-    test('/abc?x=42#xyz', '/abc', 'x=42', 'xyz')(parse('/abc?x=42#xyz'))
-    test('/abc/?x=42#xyz', '/abc/', 'x=42', 'xyz')(parse('/abc/?x=42#xyz'))
-    test('/abc#xyz', '/abc', '', 'xyz')(parse('/abc#xyz'))
-    test('/abc?xyz', '/abc', 'xyz', '')(parse('/abc?xyz'))
-    test('/?xyz#xyz', '/', 'xyz', 'xyz')(parse('/?xyz#xyz'))
-    test('?xyz#xyz', '', 'xyz', 'xyz')(parse('?xyz#xyz'))
-    test('#xyz?xyz', '', '', 'xyz?xyz')(parse('#xyz?xyz'))
-    test('/abc', '/abc', '', '')(parse('/abc'))
+    verifyMatch('/abc?x=42#xyz', '/abc', 'x=42', 'xyz')(parse('/abc?x=42#xyz'))
+    verifyMatch('/abc/?x=42#xyz', '/abc/', 'x=42', 'xyz')(parse('/abc/?x=42#xyz'))
+    verifyMatch('/abc#xyz', '/abc', '', 'xyz')(parse('/abc#xyz'))
+    verifyMatch('/abc?xyz', '/abc', 'xyz', '')(parse('/abc?xyz'))
+    verifyMatch('/?xyz#xyz', '/', 'xyz', 'xyz')(parse('/?xyz#xyz'))
+    verifyMatch('?xyz#xyz', '', 'xyz', 'xyz')(parse('?xyz#xyz'))
+    verifyMatch('#xyz?xyz', '', '', 'xyz?xyz')(parse('#xyz?xyz'))
+    verifyMatch('/abc', '/abc', '', '')(parse('/abc'))
+  })
+  it('parseHref for url', function() {
+    verifyMatch('/abc?x=42#xyz', '/abc', 'x=42', 'xyz')(u.parseHref('http://foo.com/abc?x=42#xyz'))
+  })
+  it('parseHref path encode', function() {
+    verifyMatch(
+      '/%5Eabc?x=42#xyz',
+      '/%5Eabc',
+      'x=42',
+      'xyz'
+    )(u.parseHref('http://foo.com/^abc?x=42#xyz'))
   })
 })
