2a01b117d94f2767efb348fdac7188d3253066a6
_gt3 <gt3.someday@outlook.com>
2017-06-15 19:14:59 -0500

    add decode option to parseQS, export parseQS closes #56

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { spec, miss, check, assignValues } from './router/spec'
 export { match, toggle, toggleSelected } from './router/match'
+export { parseQS } from './router/utils-path'
 export { container } from './container'
 export { Anchor } from './anchor'
