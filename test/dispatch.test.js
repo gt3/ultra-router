8899e169b0633826b4fd3d168bddeaa803d2045d
_gt3 <gt3.someday@outlook.com>
2017-06-22 23:56:35 -0500

    dispatch test with timer

--- test/dispatch.test.js
+++ test/dispatch.test.js
@@ -6,38 +6,51 @@ import { prefixSpec, spec, check, assignValues, miss } from '../src/router/spec'
 import { toggle, toggleSelected, match, prefixMatch } from '../src/router/match'
 import { dispatcher } from '../src/router/dispatch'
 
-describe('dispatcher', function() {
-  let next, fail, missed
+describe('dispatch + settle', function() {
+  let next, fail, missed, task, nextTask
   let clearMocks = () => {
     next.mockClear()
     fail.mockClear()
     missed.mockClear()
+    task.mockClear()
+    nextTask.mockClear()
   }
-  beforeEach(function() { 
+  beforeEach(function() {
     next = mock()
     fail = mock()
     missed = mock()
+    task = mock()
+    nextTask = mock(u.scheduleTask(task, true))
   })
   it('should resolve, fail, and reject upon path match', function() {
-    let d = dispatcher([
-      match(spec('/a')(next)), 
-      match(spec('/b')(next, null, fail))
-    ], [
-      miss(missed, '/a')
-    ])
-    d({path: '/a'})
+    let d = dispatcher(
+      [match(spec('/a')(next)), match(spec('/b')(next, null, fail))],
+      [miss(missed, '/a')]
+    )
+    d({ path: '/a' })
     eq(next.mock.calls.length, 1)
     eq(fail.mock.calls.length, 1)
     eq(missed.mock.calls.length, 0)
     clearMocks()
-    d({path: '/b'})
+    d({ path: '/b' })
     eq(next.mock.calls.length, 1)
     eq(fail.mock.calls.length, 0)
     eq(missed.mock.calls.length, 1)
     clearMocks()
-    d({path: '/xxx'})
+    d({ path: '/xxx' })
     eq(next.mock.calls.length, 0)
     eq(fail.mock.calls.length, 0)
     eq(missed.mock.calls.length, 0)
+  })
+  it('should run scheduled task (timer)', function(done) {
+    let d = dispatcher([match(spec('/a')(nextTask))], [miss(missed, '/c')])
+    d({ path: '/a' })
+    eq(nextTask.mock.calls.length, 1)
+    eq(task.mock.calls.length, 0)
+    eq(missed.mock.calls.length, 1)
+    setTimeout(() => {
+      eq(task.mock.calls.length, 1)
+      done()
+    })
   })
 })
