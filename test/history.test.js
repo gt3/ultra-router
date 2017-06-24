b6f9d5111e82c43c97b9f440db6d063c768e07c1
Ankit Patel <gt3.someday@outlook.com>
2017-06-23 21:47:53 -0500

    push replace tests

--- test/history.test.js
+++ test/history.test.js
@@ -8,23 +8,41 @@ import { setOrigin, setLocation } from './helpers-jsdom'
 import * as u from '../src/router/utils-path'
 
 describe('history', function() {
-  let loc = window.location, state = { x: 42 }
+  let loc = window.location, state = { x: 42 }, hlen = 0, path
+  beforeEach(function() {
+    hlen = u.env.history.length;
+    ([, path = '0'] = Math.random().toString().split('.'))
+    path = '/' + path.slice(0, 3)
+  })
   it('push', function() {
     eq(u.env.href, '/')
-    push(null, { href: '/a', path: '/a', state })
-    eq(u.env.href, '/a')
+    push(null, { href: path, path, state })
+    eq(u.env.history.length - hlen, 1)
+    eq(u.env.href, path)
     eq(u.env.history.state, state)
     push(null, { href: 'http://localhost:8080/xyz?q=42#skipto', path: '/xyz' })
+    eq(u.env.history.length - hlen, 2)
     eq(u.env.href, '/xyz?q=42#skipto')
   })
   it('should not allow duplicate push', function() {
     let cb = mock()
-    push(cb, { href: '/a', path: '/a' })
+    push(cb, { href: path, path })
+    eq(u.env.history.length - hlen, 1)
     eq(cb.mock.calls.length, 1)
-    push(cb, { href: '/a', path: '/a' }) //warning
+    push(cb, { href: path, path }) //warning
+    eq(u.env.history.length - hlen, 1)
     eq(cb.mock.calls.length, 1)
   })
   it('replace', function() {
-    
+    let cb = mock()
+    replace(cb, { href: path, path })
+    eq(u.env.history.length - hlen, 0)
+    eq(cb.mock.calls.length, 1)
+    replace(cb, { href: path, path })
+    eq(cb.mock.calls.length, 1)
+    replace(cb, { href: path, path, state })
+    eq(u.env.history.length - hlen, 0)
+    eq(cb.mock.calls.length, 2)
+    eq(u.env.history.state, state)
   })
 })
